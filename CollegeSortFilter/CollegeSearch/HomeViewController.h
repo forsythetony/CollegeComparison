56a8eb2e5318300a6adf5c58790da391140f6db6
Scott Wood <sawg62@mail.missouri.edu>
2013-11-19 02:52:11 -0600

    working on college sort filter

---
+++ CollegeSortFilter/CollegeSearch/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  CollegeSearch
+//
+//  Created by borrower on 11/17/13.
+//  Copyright (c) 2013 MUIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ CollegeSortFilter/CollegeSearch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollegeSearch
+//
+//  Created by borrower on 11/5/13.
+//  Copyright (c) 2013 MUIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CollegeSearchAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CollegeSearchAppDelegate class]));
+    }
+}
