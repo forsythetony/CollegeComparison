6bf9afe22d5ff35567363da6261bd41daa7c9475
Scott Wood <sawg62@mail.missouri.edu>
2013-11-25 21:55:36 -0600

    added states list

---
+++ CollegeComparison/StateTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  StateTableViewController.h
+//  CollegeComparison
+//
+//  Created by borrower on 11/25/13.
+//  Copyright (c) 2013 MUIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StateTableViewController : UIViewController
+
+@end
