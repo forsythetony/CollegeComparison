c594a8f9b83b35f6d42646aa74424baa4128d556
Fernando C <compsci@mwc-088105.dhcp.missouri.edu>
2013-12-03 11:42:16 -0600

    Updated Details Page

---
+++ CollegeComparison/CollegeDetailTableViewController.h
@@ -0,0 +1,25 @@
+//
+//  CollegeDetailTableViewController.h
+//  CollegeComparison
+//
+//  Created by Fernando Colon on 12/2/13.
+//  Copyright (c) 2013 MUIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollegeDetailTableViewController : UITableViewController
+
+@property (nonatomic, strong) IBOutlet UILabel *collegeLabel;
+@property (nonatomic, strong) NSString *collegeName;
+@property (weak, nonatomic) IBOutlet UILabel *locationLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tuitionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *studentBTotalLabel;
+@property (weak, nonatomic) IBOutlet UILabel *menEnrollLabel;
+@property (weak, nonatomic) IBOutlet UILabel *womenEnrollLabel;
+@property (weak, nonatomic) IBOutlet UILabel *finaidLabel;
+@property (weak, nonatomic) IBOutlet UILabel *institLabel;
+@property (weak, nonatomic) IBOutlet UILabel *degreeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *accRateLabel;
+
+@end
---
+++ CollegeComparison/CollegeDetailTableViewController.m
@@ -0,0 +1,94 @@
+//
+//  CollegeDetailTableViewController.m
+//  CollegeComparison
+//
+//  Created by Fernando Colon on 12/2/13.
+//  Copyright (c) 2013 MUIT. All rights reserved.
+//
+
+#import "CollegeDetailTableViewController.h"
+
+@interface CollegeDetailTableViewController ()
+
+@end
+
+
+@implementation CollegeDetailTableViewController
+
+@synthesize collegeLabel;
+@synthesize collegeName;
+@synthesize locationLabel;
+@synthesize tuitionLabel;
+@synthesize studentBTotalLabel;
+@synthesize menEnrollLabel;
+@synthesize womenEnrollLabel;
+@synthesize finaidLabel;
+@synthesize institLabel;
+@synthesize degreeLabel;
+@synthesize accRateLabel;
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    [super viewDidLoad];
+	// Set the Label text with the selected recipe
+    collegeLabel.text = collegeName;
+    tuitionLabel.text = @"40,000";
+    studentBTotalLabel.text = @"30,000";
+    
+    float menEnrollment = 12000, totalEnrollment = 30000, womenEnrollment = 18000;
+    float menPerc = menEnrollment/totalEnrollment, womenPerc = womenEnrollment/totalEnrollment;
+    
+    menEnrollLabel.text = [NSString stringWithFormat:@"%.2f%%", (menPerc*100)];
+    
+    womenEnrollLabel.text = [NSString stringWithFormat:@"%.2f%%", (womenPerc*100)];
+    
+    finaidLabel.text = @"70%";
+    institLabel.text = @"Public";
+    degreeLabel.text = @"Associates";
+    accRateLabel.text = @"60%";
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+@end
