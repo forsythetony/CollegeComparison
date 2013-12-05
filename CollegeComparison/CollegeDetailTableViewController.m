c2379bc70ceb27bd8ac30b52fffeede93f00eada
Fernando Colon <flcb7d@mail.missouri.edu>
2013-12-04 23:15:43 -0600

    added N/A’s to the admission requirements

--- CollegeComparison/CollegeDetailTableViewController.m
+++ CollegeComparison/CollegeDetailTableViewController.m
@@ -16,7 +16,7 @@
 @implementation CollegeDetailTableViewController
 
 @synthesize collegeLabel;
-@synthesize collegeName;
+@synthesize instituteType;
 @synthesize locationLabel;
 @synthesize inTuitionLabel;
 @synthesize outTuitionLabel;
@@ -50,15 +50,20 @@
     
     MUITCollege *college = self.representedCollege;
     
-    collegeLabel.text = college.name;
-    locationLabel.text = college.state;
-    inTuitionLabel.text = [NSString stringWithFormat:@"$%d", college.tuition_in_state];
-    outTuitionLabel.text = [NSString stringWithFormat:@"$%d", college.tuition_out_state];
+    collegeLabel.text = college.name;   //Displays the name of the university/college selected
+    locationLabel.text = college.state; //Displays the state in 2 letter format
     
+    if (college.tuition_in_state <= 0) inTuitionLabel.text = @"N/A"; //checks to make sure value exists
+    else inTuitionLabel.text = [NSString stringWithFormat:@"$%d", college.tuition_in_state];
+    
+    if (college.tuition_out_state <= 0) outTuitionLabel.text = @"N/A";
+    else outTuitionLabel.text = [NSString stringWithFormat:@"$%d", college.tuition_out_state];
+    
+    //logic for making the percentages of the enrollment of men and women
     float menEnrollment = college.enrollment_men, totalEnrollment = college.enrollment_total, womenEnrollment = college.enrollment_women;
     float menPerc = menEnrollment/totalEnrollment, womenPerc = womenEnrollment/totalEnrollment;
 
-    
+    //error checking then displays
     if (college.enrollment_total <= 0) studentBTotalLabel.text = @"N/A";
     else studentBTotalLabel.text = [NSString stringWithFormat:@"%d", college.enrollment_total];
     
@@ -71,18 +76,36 @@
     if (college.percent_receive_financial_aid <= 0) finaidLabel.text = @"N/A";
     else finaidLabel.text = [NSString stringWithFormat:@"%d%%", college.percent_receive_financial_aid];
     
-    institLabel.text = @"Public";
-    accRateLabel.text = @"60%";
+    //logic to determine the institution type
+    if (college.control == 1) instituteType = @"Public";
+    else if (college.control == 2) instituteType = @"Private";
+    else instituteType  = @"N/A";
+    institLabel.text = instituteType;
     
-    actRead.text = [NSString stringWithFormat:@"%d - %d", college.act_english_25, college.act_english_75];
-    actMath.text = [NSString stringWithFormat:@"%d - %d", college.act_math_25, college.act_math_75];
-    actWriting.text = [NSString stringWithFormat:@"%d - %d", college.act_writing_25, college.act_writing_75];
+    //awesome acceptance rates
+    accRateLabel.text = @".007%";
+    
+    //the following if statements display N/A's if there is no score for each text section
+    if (college.act_english_25 <= 0 && college.act_english_75 <= 0) actRead.text = @"N/A";
+    else actRead.text = [NSString stringWithFormat:@"%d - %d", college.act_english_25, college.act_english_75];
+    
+    if (college.act_math_25 <= 0 && college.act_math_75 <= 0) actMath.text = @"N/A";
+    else actMath.text = [NSString stringWithFormat:@"%d - %d", college.act_math_25, college.act_math_75];
+    
+    if (college.act_writing_25 <= 0 && college.act_writing_75 <= 0) actWriting.text = @"N/A";
+    else actWriting.text = [NSString stringWithFormat:@"%d - %d", college.act_writing_25, college.act_writing_75];
+    
+    if (college.act_25 <= 0 && college.act_75 <= 0) actComposite.text = @"N/A";
     actComposite.text = [NSString stringWithFormat:@"%d - %d", college.act_25, college.act_75];
     
-    satRead.text = [NSString stringWithFormat:@"%d - %d", college.sat_reading_25, college.sat_reading_75];
-    satMath.text = [NSString stringWithFormat:@"%d - %d", college.sat_math_25, college.sat_math_75];
-    satWriting.text = [NSString stringWithFormat:@"%d - %d", college.sat_writing_25, college.sat_writing_75];
-//    }
+    if (college.sat_reading_25 <= 0 && college.sat_reading_75 <= 0) satRead.text = @"N/A";
+    else satRead.text = [NSString stringWithFormat:@"%d - %d", college.sat_reading_25, college.sat_reading_75];
+    
+    if (college.sat_math_25 <= 0 && college.sat_math_75 <= 0) satMath.text = @"N/A";
+    else satMath.text = [NSString stringWithFormat:@"%d - %d", college.sat_math_25, college.sat_math_75];
+    
+    if (college.sat_writing_25 <= 0 && college.sat_writing_75 <= 0) satWriting.text = @"N/A";
+    else satWriting.text = [NSString stringWithFormat:@"%d - %d", college.sat_writing_25, college.sat_writing_75];
 }
 
 - (void)didReceiveMemoryWarning
