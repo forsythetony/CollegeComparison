16b58d53066a196470aa3cb0da612cd5914677d3
forsythetony <forsythetony@gmail.com>
2014-02-04 06:23:10 -0600

    A little cleanup

---
+++ CollegeComparison/search/CollegeSearchTableView.m
@@ -0,0 +1,31 @@
+//
+//  CollegeSearchTableView.m
+//  CollegeComparison
+//
+//  Created by Anthony Forsythe on 2/4/14.
+//  Copyright (c) 2014 MUIT. All rights reserved.
+//
+
+#import "CollegeSearchTableView.h"
+
+@implementation CollegeSearchTableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
--- CollegeComparison/search/FilteredCollegesViewController.m
+++ CollegeComparison/search/FilteredCollegesViewController.m
@@ -46,20 +46,36 @@ NSArray *searchResults;
     [self setCustomAttributesForNavigationBar];
     
     // Navigation bar buttons
-    self.selectButton = [[UIBarButtonItem alloc] initWithTitle:@"Select" style:UIBarButtonItemStyleBordered target:self action:@selector(selectButton:)];
+    self.selectButton = [[UIBarButtonItem alloc] initWithTitle:@"Select"
+                                                         style:UIBarButtonItemStyleBordered
+                                                        target:self
+                                                        action:@selector(selectButton:)];
+    
     self.selectButton.tintColor = [UIColor whiteColor];
     
-    self.compareButton = [[UIBarButtonItem alloc] initWithTitle:@"Compare" style:UIBarButtonItemStyleBordered target:self action:@selector(selectCollegesToCompare:)];
+    self.compareButton = [[UIBarButtonItem alloc] initWithTitle:@"Compare"
+                                                          style:UIBarButtonItemStyleBordered
+                                                         target:self
+                                                         action:@selector(selectCollegesToCompare:)];
+
     self.compareButton.tintColor = [UIColor whiteColor];
+    
     self.compareButton.enabled = NO;
     
-    self.cancelButton = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStyleBordered target:self action:@selector(cancelButton:)];
+    self.cancelButton = [[UIBarButtonItem alloc] initWithTitle:@"Cancel"
+                                                         style:UIBarButtonItemStyleBordered
+                                                        target:self
+                                                        action:@selector(cancelButton:)];
+ 
     self.cancelButton.tintColor = [UIColor whiteColor];
     
     [self.navigationItem setRightBarButtonItem:self.selectButton];
     
     // Changing the checkmark color while selecting universities
-    [self.tableView setTintColor:[UIColor colorWithRed:240.0/255.0 green:87.0/255.0 blue:70.0/255.0 alpha:1.0]];
+    [self.tableView setTintColor:[UIColor colorWithRed:240.0/255.0
+                                                 green:87.0/255.0
+                                                  blue:70.0/255.0
+                                                 alpha:1.0]];
     
     self.tableView.allowsMultipleSelectionDuringEditing = YES;
 }
@@ -81,11 +97,11 @@ NSArray *searchResults;
     [self.navigationItem setRightBarButtonItem:self.cancelButton animated:YES];
     
     [self.navigationItem setLeftBarButtonItem:self.compareButton animated:YES];
+    
     self.compareButton.title = @"Compare";
     
     [self.tableView setEditing:YES animated:YES];
 }
-
 // Go back to main table view
 - (void) cancelButton:(id)sender
 {
@@ -96,9 +112,13 @@ NSArray *searchResults;
     // Make all visible cells interactable
     for (int row = 0; row < [self.tableView numberOfRowsInSection:0]; row++)
     {
-        NSIndexPath* cellPath = [NSIndexPath indexPathForRow:row inSection:0];
+        NSIndexPath* cellPath = [NSIndexPath indexPathForRow:row
+                                                   inSection:0];
+        
         UITableViewCell* cell = [self.tableView cellForRowAtIndexPath:cellPath];
+        
         cell.alpha = 1.0;
+        
         cell.userInteractionEnabled = YES;
     }
     
@@ -115,7 +135,6 @@ NSArray *searchResults;
     return 1;
 }
 
-
 // Table view header
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
@@ -123,14 +142,26 @@ NSArray *searchResults;
     NSUInteger collegesReturned = self.universitiesPassed.count;
     
     UIView *tableHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 320.0f, 40.0f)];
-    [tableHeaderView setBackgroundColor:[UIColor colorWithRed:245.0/255 green:245.0/255 blue:245.0/255 alpha:1.0]];
+    
+    [tableHeaderView setBackgroundColor:[UIColor colorWithRed:245.0/255
+                                                        green:245.0/255
+                                                         blue:245.0/255
+                                                        alpha:1.0]];
     
     // Label properties for custon UIView
     UILabel *labelInHeaderView = [[UILabel alloc] initWithFrame:CGRectMake(20.0f, 0.0f, 320.0f, 40.0f)];
+    
     labelInHeaderView.textAlignment = NSTextAlignmentLeft;
-    [labelInHeaderView setBackgroundColor:[UIColor colorWithRed:245.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0]];
+    
+    [labelInHeaderView setBackgroundColor:[UIColor colorWithRed:245.0/255.0
+                                                          green:245.0/255.0
+                                                           blue:245.0/255.0
+                                                          alpha:1.0]];
+    
     [labelInHeaderView setTextColor:[UIColor grayColor]];
+    
     labelInHeaderView.font = [UIFont fontWithName:@"Avenir-Book" size:13.0];
+    
     labelInHeaderView.text = [[NSString stringWithFormat:@"%lu", (unsigned long)collegesReturned] stringByAppendingString:@" COLLEGES RETURNED"];
     
     // Add label to the view
@@ -159,7 +190,6 @@ NSArray *searchResults;
 // Tableview cell creation
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    //MUITCollege *college = self.representedCollege;
     
     // Local variables
     static NSString *cellIdentifier = @"CollegeCell";
@@ -171,28 +201,46 @@ NSArray *searchResults;
     }
     
     // Cell label customization
-    [cell.universityNameLabel setTextColor:[UIColor colorWithRed:0.0/255 green:174.0/255 blue:239.0/255 alpha:1.0]];
+    [cell.universityNameLabel setTextColor:[UIColor colorWithRed:0.0/255
+                                                           green:174.0/255
+                                                            blue:239.0/255
+                                                           alpha:1.0]];
+    
     [cell.universityNameLabel setFont:[UIFont fontWithName:@"Avenir-Heavy" size:17.0]];
     
-    [cell.universityLocationLabel setTextColor:[UIColor colorWithRed:147.0/255.0 green:149.0/255 blue:152.0/255 alpha:1.0]];
+    [cell.universityLocationLabel setTextColor:[UIColor colorWithRed:147.0/255.0
+                                                               green:149.0/255
+                                                                blue:152.0/255
+                                                               alpha:1.0]];
+    
     [cell.universityLocationLabel setFont:[UIFont fontWithName:@"Avenir-Book" size:17.0]];
     
-    [cell.universityTuitionLabel setTextColor:[UIColor colorWithRed:147.0/255.0 green:149.0/255 blue:152.0/255 alpha:1.0]];
+    [cell.universityTuitionLabel setTextColor:[UIColor colorWithRed:147.0/255.0
+                                                              green:149.0/255
+                                                               blue:152.0/255
+                                                              alpha:1.0]];
+    
     [cell.universityTuitionLabel setFont:[UIFont fontWithName:@"Avenir-Book" size:17.0]];
     
     NSNumberFormatter *tuition = [[NSNumberFormatter alloc] init];
     [tuition setFormatterBehavior:NSNumberFormatterBehavior10_4];
     [tuition setNumberStyle:NSNumberFormatterCurrencyStyle];
+    
     NSNumber *amount = [NSNumber numberWithInteger:[(MUITCollege *)[self.universitiesPassed objectAtIndex:indexPath.row] tuition_out_state]];
     
     if (tableView == self.searchDisplayController.searchResultsTableView) {
+        
         cell.universityNameLabel.text = [(MUITCollege *)[searchResults objectAtIndex:indexPath.row] name];
+        
         cell.universityLocationLabel.text = [(MUITCollege *)[self.universitiesPassed objectAtIndex:indexPath.row] state];
+        
         cell.universityTuitionLabel.text = [tuition stringFromNumber:amount];
     }
     else {
         cell.universityNameLabel.text = [(MUITCollege *)[self.universitiesPassed objectAtIndex:indexPath.row] name];
+        
         cell.universityLocationLabel.text = [(MUITCollege *)[self.universitiesPassed objectAtIndex:indexPath.row] state];
+        
         cell.universityTuitionLabel.text = [tuition stringFromNumber:amount];
     }
     
