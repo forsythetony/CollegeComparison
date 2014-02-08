3cee30129d180ae89d0aca112fdb39efec13c579
forsythetony <forsythetony@gmail.com>
2014-02-08 16:35:24 -0600

    Working layout and default layout function for home page

--- CollegeComparison/home/newHomeFiltersViewController.h
+++ CollegeComparison/home/newHomeFiltersViewController.h
@@ -12,6 +12,32 @@
 #import <MapKit/MapKit.h>
 #import <CoreLocation/CoreLocation.h>
 
-@interface newHomeFiltersViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+@interface newHomeFiltersViewController : UIViewController
+
+//Header view
+@property (weak, nonatomic) IBOutlet UIImageView *headerImageView;
+@property (weak, nonatomic) IBOutlet UIView *collegeCountContainerView;
+
+//Locations picker
+@property (weak, nonatomic) IBOutlet UIView *locationsContainerView;
+@property (weak, nonatomic) IBOutlet UILabel *locationsTitleLabel;
+@property (weak, nonatomic) IBOutlet UITableView *locationsTableView;
+
+//Tuition Slider
+@property (weak, nonatomic) IBOutlet UIView *tuitionContainerView;
+@property (weak, nonatomic) IBOutlet UILabel *tuitionLabel;
+@property (weak, nonatomic) IBOutlet UISlider *tuitionSlider;
+
+//Enrollment Slider
+@property (weak, nonatomic) IBOutlet UIView *enrollmentContainerView;
+@property (weak, nonatomic) IBOutlet UILabel *enrollmentLabel;
+@property (weak, nonatomic) IBOutlet UISlider *enrollmentSlider;
+
+//School Type
+@property (weak, nonatomic) IBOutlet UIView *schoolTypeContainerView;
+@property (weak, nonatomic) IBOutlet UILabel *schoolTypeLabel;
+@property (weak, nonatomic) IBOutlet UIButton *publicSchoolButton;
+@property (weak, nonatomic) IBOutlet UIButton *privateSchoolButton;
+
 
 @end
